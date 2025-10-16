ea9d90b10657cd17b2a606b9e134fc25f4a1bbfd
andrazmen <and.menegatti@gmail.com>
2025-10-16 11:41:22 +0200

    backend url update

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:5000";
+//const url = "http://localhost:5000";
+//const url = "https://log-monitor-client.onrender.com/";
 
 export const customFetch = axios.create({
   baseURL: url,
