e294f4a71995758d5cc34cde89cd7b90bee6f96b
andrazmen <and.menegatti@gmail.com>
2025-10-16 15:38:18 +0200

    db creds upd

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,15 +2,16 @@ const mysql = require("mysql2");
 
 const dotenv = require("dotenv");
 
-//dotenv.config();
-/*
+dotenv.config();
+
 const conn = mysql.createPool({
   host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   connectionLimit: 15,
 });
-*/
-const conn = mysql.createPool(process.env.MYSQL_URL);
+
+//const conn = mysql.createPool(process.env.MYSQL_URL);
 exports.pool = conn;
